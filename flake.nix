427b042dfde2b29b599eda54aeae49da4d517f8d
Cody Tucker <cody@tmvsocial.com>
2026-01-04 16:41:57 -0600

    Change nixpkgs input to nixos-24.11 for stability

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A Nix-flake-based Python document uploader for Qdrant";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
   outputs =
     { self, nixpkgs }:
